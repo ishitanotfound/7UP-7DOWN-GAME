ad93845dace925cdd89698f98c41d327baa3c249
Ishita Singh <2114ishita@gmail.com>
2025-06-27 22:40:42 +0530

    made result msg and p adjust

--- src/Components/MainGame/MainGame.jsx
+++ src/Components/MainGame/MainGame.jsx
@@ -96,7 +96,7 @@ export default function MainGame({showConfetti, setShowConfetti, track, setTrack
       </div>
       
       {/* amt input box */}
-      <div className="flex justify-center mt-4 text-md lg:text-2xl text-white ">
+      <div className="flex justify-center mt-3 text-md lg:text-2xl text-white ">
         <input type="text" placeholder="Enter betting amount here..." className="input input-ghost text-center pl-1 w-[15rem] lg:w-sm" onChange={(e) => {
           if(e.target.value != "" && e.target.value > 0) {                    // issue
             setAmt(e.target.value); 
@@ -107,7 +107,7 @@ export default function MainGame({showConfetti, setShowConfetti, track, setTrack
       </div>  
       
       {/* diceboard */}
-      <div className="diceBoard flex justify-center items-center text-white text-2xl gap-15 bg-[#414040] w-[80%] lg:w-[60%] rounded-2xl mx-auto mt-4 pb-4">
+      <div className="diceBoard flex justify-center items-center text-white text-2xl gap-15 bg-[#414040] w-[80%] lg:w-[60%] rounded-2xl mx-auto mt-3 pb-4">
         <div className='dice flex flex-col items-center'>
           <h1 id="dice1" className={`${isAnimating ? 'animate' : ''}`}>{ dice1 }</h1>
           <label htmlFor="dice1">Dice1</label>
@@ -126,7 +126,7 @@ export default function MainGame({showConfetti, setShowConfetti, track, setTrack
       {/* result box */}
       <div className="result flex flex-col items-center justify-center m-3 text-gray-300 pb-3">
         <h3>{ msg }</h3>
-        <p className="text-gray-200 text-center hover:cursor-default">Press the top right button to veiw Scores.</p>
+        <p className=" text-gray-200 text-center text-xs hover:cursor-default">Press the top right button to veiw Scores.</p>
       </div>  
       
     </div>
